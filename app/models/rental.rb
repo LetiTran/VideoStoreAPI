510e056cbe5d131cd8d09f3b35beecf54aa0cae7
Madaleine Shields <maddiev.shields@gmail.com>
2018-05-07 16:52:43 -0700

    Add methods to Rental Model

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,8 +1,20 @@
 class Rental < ApplicationRecord
+  require 'date'
+
+  ONE_WEEK = 7
 
   validates :due_date, :returned, :customer_id, :movie_id, presence: true
 
   belongs_to :costumer
   belongs_to :movie
 
+  def assign_due_date
+    self.due_date = Date.today + ONE_WEEK
+  end
+
+  def check_in
+    if self.returned == false
+      self.update_attributes(returned: true)
+    end
+  end
 end
