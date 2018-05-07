43675980291a4661cc1c9592561aa39eb4a4d4da
LetiTran <leticiastran@gmail.com>
2018-05-07 16:18:11 -0700

    Added controller methods

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,9 @@ class CustomersController < ApplicationController
   def index
   end
 
+  def create
+  end
+
   def zomg
     message = "it works"
     render json: message.as_json
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,5 @@ Rails.application.routes.draw do
 
   resources :rentals, only: [:index, :show, :create, :update]
 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
