0303b720390eb668f1bd6b54068f32c9d6357195
LetiTran <leticiastran@gmail.com>
2018-05-07 16:41:06 -0700

    Added method to return movies checked out by costumer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,8 @@ class Customer < ApplicationRecord
   validates :name, :registered_at, :phone, :address, :city, :state, :postal_code, presence: true
 
   has_many :rentals
+
+  def movies_checked_out_count
+    return Rental.where(customer_id: self.id, returned: false).count
+  end
 end
