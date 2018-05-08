b76914fbb6f0952be892e5a936fd708ecb5aeeea
Madaleine Shields <maddiev.shields@gmail.com>
2018-05-08 14:20:52 -0700

    Write tests for Rental model

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -2,17 +2,72 @@ require "test_helper"
 require "date"
 
 describe Rental do
-  before do
-    rental = {
-      "due_date" => Date.today + 7,
-      "returned" => false,
-      "customer" => customers(:shelley),
-      "movie" => movies(:blacksmith)
-    }
-    @new_rental = Rental.new(rental)
+  let(:rental) { Rental.new }
+
+  describe "validations" do
+    it "must be valid" do
+      rental_one = rentals(:rental_one)
+      rental_one.valid?.must_equal true
+    end
+
+    it "must be invalid if returned is true" do
+      rental_two = rentals(:rental_two)
+      rental_two.valid?.must_equal false
+    end
+
+    it "must be invalid if due date, customer, and movie are blank" do
+      rental_three = rentals(:rental_three)
+      rental_three.valid?.must_equal false
+    end
   end
 
-  it "must be valid" do
-    value(@new_rental).must_be :valid?
+  describe "relations" do
+    it "must have a Customer" do
+      rental_one = rentals(:rental_one)
+      rental_one.must_respond_to :customer
+      rental_one.customer.must_be_kind_of Customer
+      rental_one.customer.name.must_equal "Shelley Rocha"
+      rental_one.customer.must_equal customers(:shelley)
+    end
+
+    it "must have a Movie" do
+      rental_one = rentals(:rental_one)
+      rental_one.must_respond_to :movie
+      rental_one.movie.must_be_kind_of Movie
+      rental_one.movie.title.must_equal "Blacksmith Of The Banished"
+      rental_one.movie.must_equal movies(:blacksmith)
+    end
   end
+
+  describe "assign_due_date" do
+    let(:rental) { rentals(:rental_one) }
+
+    it "can assign a due date" do
+      rental.due_date = nil
+      rental.due_date.must_equal nil
+      rental.assign_due_date
+      rental.due_date.must_be_kind_of Date
+    end
+
+    it "can assign a due date 7 days from Rental creation" do
+      rental.due_date = nil
+      rental.due_date.must_equal nil
+      rental.assign_due_date
+      rental.due_date.must_equal rental.created_at.to_date + 7
+    end
+  end
+
+  describe "check_in" do
+    it "can check a movie back in" do
+      rental.returned == false
+      rental.returned.must_equal false
+      rental.check_in
+      rental.returned.must_equal true
+
+    end
+
+  end
+
+
+
 end
