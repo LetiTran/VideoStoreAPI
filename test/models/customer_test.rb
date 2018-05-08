45e6609f3e99cc60aabf74f94f4712a8dc4653e2
LetiTran <leticiastran@gmail.com>
2018-05-08 10:30:01 -0700

    Fixed fixtures

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,31 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  describe 'validates' do
+
+    let(:shelly) { customers(:shelly) }
+
+    it "must be valid" do
+      value(shelly).must_be :valid?
+    end
+
+    it "must have a name" do
+
+    end
+
+    it "must have a registered_at date" do
+
+    end
+
+    it "must have a phone" do
+
+    end
+
+
+
   end
 end
+
+
+# validates :name, :registered_at, :phone, :address, :city, :state, :postal_code, presence: true
