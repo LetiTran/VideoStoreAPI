0330f7ded7b7f23be9f487750acd6e906ef2d9e5
LetiTran <leticiastran@gmail.com>
2018-05-08 10:59:23 -0700

    Fixed typo in Rental model

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -5,7 +5,7 @@ class Rental < ApplicationRecord
 
   validates :due_date, :returned, :customer_id, :movie_id, presence: true
 
-  belongs_to :costumer
+  belongs_to :customer
   belongs_to :movie
 
   def assign_due_date
